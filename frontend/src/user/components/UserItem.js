ad758025ff2fcfcaf050af284f7c4974da7fca60
tanvir1418 <tanvirahmed1418@gmail.com>
2021-12-13 17:53:01 +0600

    Adding a UsersList Page  Component

---
+++ frontend/src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = (props) => {
+  return <div></div>;
+};
+
+export default UserItem;
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "ui",
+      name: "Tanvir Ahmed",
+      image:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fprofile%2520picture%2F&psig=AOvVaw29m1LgIUV8gGcX8fuNYs2K&ust=1639482569652000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNji7tba4PQCFQAAAAAdAAAAABAD",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;

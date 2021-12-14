6520a91c8bfe8893d12f61ee3a222746963e74b7
tanvir1418 <tanvirahmed1418@gmail.com>
2021-12-14 19:45:36 +0600

    Rendering User Places and Dynamic Routes

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Shat Gambuj Mosque",
+    description:
+      "Shat Gambuj Masjid or the Sixty Dome Mosque resides in the city of mosque, Bagerhat of Khulna division. It’s the largest mosque in Bangladesh and registered under the UNESCO World Heritage Site.",
+    imageUrl:
+      "https://www.gosahin.com/go/p/j/1577910315_sixty-dome-moque-1.jpg",
+    address: "Bagerhat - Khulna Rd, 9300",
+    creator: "u1",
+    location: {
+      lat: 22.6745068,
+      lng: 89.7418359,
+    },
+  },
+  {
+    id: "p2",
+    title: "Lalbagh Fort",
+    description:
+      "This famous historic place is situated on the northeast side of Dhaka, Bangladesh. This was built during the Mughal rule, on seventeenth century.",
+    imageUrl: "https://live.staticflickr.com/4228/34129364493_4b3e4bb0ba_b.jpg",
+    address: "Lalbagh Rd, Dhaka 1211",
+    creator: "u2",
+    location: {
+      lat: 23.7188605,
+      lng: 90.3881596,
+    },
+  },
+  {
+    id: "p3",
+    title: "Ahsan Manzil",
+    description:
+      "This is the heritage building of the then jamindar of Jamalpur. It was built during the late eighteenth century by Sheik Enayet Ullah.",
+    imageUrl: "https://live.staticflickr.com/65535/7707182500_947e915528_b.jpg",
+    address: "Ahsanullah Rd, Dhaka 1100",
+    creator: "u3",
+    location: {
+      lat: 23.7085915,
+      lng: 90.4060074,
+    },
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;

c3d93da9930cbea5be743801ba3578090d9bfd70
tanvir1418 <tanvirahmed1418@gmail.com>
2021-12-22 20:07:11 +0600

    Getting Places by the UserID

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -69,12 +69,19 @@ const getPlaceById = async (req, res, next) => {
   res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  const places = DUMMY_PLACES.filter((p) => {
-    return p.creator === userId;
-  });
+  let places;
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching places failed, please try again later",
+      500
+    );
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
     return next(
@@ -82,7 +89,10 @@ const getPlacesByUserId = (req, res, next) => {
     );
   }
 
-  res.json({ places });
+  // find({}) returns an array so that at first we have to map them and then convert them to object
+  res.json({
+    places: places.map((place) => place.toObject({ getters: true })),
+  });
 };
 
 const createPlace = async (req, res, next) => {

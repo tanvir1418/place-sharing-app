6dacf203372914aec917583f6f25deb2215acba5
tanvir1418 <tanvirahmed1418@gmail.com>
2021-12-23 21:14:05 +0600

    Sending a Login Request

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -63,6 +63,31 @@ const Auth = () => {
     event.preventDefault();
 
     if (isLoginMode) {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
+      }
     } else {
       try {
         setIsLoading(true);
@@ -84,11 +109,9 @@ const Auth = () => {
           throw new Error(responseData.message);
         }
 
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || "Something went wrong, please try again.");
       }
